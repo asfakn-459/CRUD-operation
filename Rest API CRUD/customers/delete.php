938532a88b2866f51c29add8f4d1dbffc76f9ed5
asfakn-459 <147117318+asfakn-459@users.noreply.github.com>
2023-11-07 08:05:16 +0530

    Add files via upload

---
+++ Rest API CRUD/customers/delete.php
@@ -0,0 +1,29 @@
+<?php
+
+header('Access-Control-Allow-Origin:*');
+header('Content-Type: application/json');
+header('Action-Control-Allow-Method: DELETE');
+header('Access-COntrol-Allow: Content-Type,Access-Control-Allow-Header, Authorization, x-Request-with');
+
+include('function.php');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if($requestMethod == "DELETE"){
+
+    
+
+        $deleteCustomer = deleteCustomer($_GET);
+        echo $deleteCustomer;
+}
+else{
+    $data = [
+        'status' => 405,
+        'message'=> $requestMethod. ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+
+}
+?>
+
